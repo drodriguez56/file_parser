2255bcd1018287306cdd4e395d35b6833c73e7be
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-03-21 17:08:58 -0400

    Add export file method to Parser module

--- parser.rb
+++ parser.rb
@@ -4,4 +4,15 @@ module Parser
     File.readlines(file)
   end
 
+  def self.export options
+    File.new("output.txt", "w+")
+    File.open("output.txt", "a") do |file|
+      (1..options.length).each do |num|
+        file.write("Output #{num}: \n")
+        file.puts(options[num-1])
+        file.write("\n")
+      end
+    end
+  end
+
 end
