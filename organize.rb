4fe968b8d44e15fc631b8430653fae971fb10f7c
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-03-21 17:39:14 -0400

    Clear lists of separators and split them into arrays

--- organize.rb
+++ organize.rb
@@ -7,3 +7,15 @@ files = []
 input_files.each do |file|
   files << Parser.import(file)
 end
+users_list = []
+files.each do |file|
+  file.each do |person|
+    if person.include?(",")
+      users_list << person.split(", ")
+    elsif person.include?(" | ")
+      users_list << person.split(" | ")
+    else person.include?(" ")
+      users_list << person.split(" ")
+    end
+  end
+end
